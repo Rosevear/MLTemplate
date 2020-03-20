ff32528d60c95abca190ea1ced34d61077ee2ea4
Cody Rosevear <CRosevear@harriscomputer.com>
2020-03-20 10:18:09 -0600

    Fresh cookiecutter template install

---
+++ VEEProtoRepo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='REsearch prototype for VEE exception automation',
+    author='Harris Utilities Smartworks',
+    license='',
+)
