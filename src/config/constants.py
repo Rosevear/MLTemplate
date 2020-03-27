a73177d4520ab339efaf85f17307229f8b906092
Cody Rosevear <CRosevear@harriscomputer.com>
2020-03-27 16:58:06 -0300

    Start data preprocessing code

--- src/config/constants.py
+++ src/config/constants.py
@@ -5,7 +5,7 @@ ROOT_DIR = Path(__file__).resolve().parents[2]
 RAW_DATA_DIR = ROOT_DIR / "data/raw"
 INTERIM_DATA_DIR = ROOT_DIR / "data/interim"
 PROCESSED_DATA_DIR = ROOT_DIR /  "data/processed"
-CUR_RAW_DATA_FILE = "BTUAcceptedSample.csv"
+CUR_RAW_DATA_FILE = "BTUSampleTotal.csv"
 CUR_PROCESSED_DATA_FILE = ""
 DATA_LOGFILE = ROOT_DIR / "logs" / "data.log"
 
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
 from pathlib import Path
-from pandas import read_csv
 import logging
 import config
 import utils
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 
 
 """
@@ -20,24 +21,51 @@ def load_csv(path):
 
     logger.info('Loading data set {}'.format(path))
     try:
-        return read_csv(path, header=0, squeeze=True)
+        return pd.read_csv(path, header=0, squeeze=True)
     except Exception as e:
         logger.info('There was a problem loading the data set {}: {}'.format(path, str(e)))
+
+def process_data(data):
+    """
+    Processes the provided pandas dataframe object by:
+    
+    Parsing the dates into separate columns and deleting the original READ_DTM column
+    Removing the METER_ID column
+    """
+
+    try:
+        data['DAY'] = pd.DatetimeIndex(data['READ_DTM']).day
+        data['MONTH'] = pd.DatetimeIndex(data['READ_DTM']).month
+        del data['READ_DTM']
+        del data['METER_ID']
+        return data
+    except Exception as e:
+        logger.info(
+            'There was a problem processing the data set {}: {}'.format(path, str(e)))
 
 if __name__ == '__main__':
 
     #Set random seeds for reproducibility
     utils.freeze_random_generators(config.RANDOM_SEED)
 
+    #Logging setup
     logging.basicConfig(filename=config.DATA_LOGFILE,
                         level=logging.INFO, format=config.LOG_FORMAT)
     logger = logging.getLogger(__name__)
 
+    #Load raw Data
     data_source = config.RAW_DATA_DIR / config.CUR_RAW_DATA_FILE
-
     logger.info('Starting the data processing pipeline for the data set {}'.format(
         config.CUR_RAW_DATA_FILE))
-    data_frame = load_csv(data_source)
+    
+    data = load_csv(data_source)
+    data = process_data(data)
+    
+    print("Fetching the first few rows of data to display...")
+    print(data.head(10))
+
+    #Sav the transformed data set
+    data.to_csv(config.PROCESSED_DATA_DIR)
 
 
 
